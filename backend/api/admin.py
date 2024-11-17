bac23b763850e357a905a2b1d55dbbae78b22f01
unknown <vitormartinscarvalho@gmail.com>
2024-11-17 00:18:37 -0300

    is good for today

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
 from .models import Note, Order
 
+
 admin.site.register(Note)
-admin.site.register(Order)
+#admin.site.register(Order)
+
+def delete_all_orders(modeladmin, request, queryset):
+    # Deletar todas as ordens
+    queryset.all().delete()
+    # Retornar uma mensagem de sucesso
+    modeladmin.message_user(request, "Todas as ordens foram deletadas com sucesso!")
+
+# Registrar o modelo no admin com a ação personalizada
+class OrderAdmin(admin.ModelAdmin):
+    actions = [delete_all_orders]  # Adiciona a ação personalizada de deletar todas as ordens
+
+admin.site.register(Order, OrderAdmin)
 
 
 # Register your models here.
